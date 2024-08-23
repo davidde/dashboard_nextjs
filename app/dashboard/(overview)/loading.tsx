ff4771cde44d210d8a4d6ff4c674d57766227a77
David Deprost <dadeprost@gmail.com>
2024-08-23 20:57:57 +0200

    Fix warning caused by loading.tsx not having a 'use client' directive

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,3 +1,6 @@
+"use client"; // Fix Warning: Each child in a list should have a unique "key" prop.
+// (https://github.com/vercel/next.js/discussions/68430#discussioncomment-10366260)
+
 import DashboardSkeleton from '@/app/ui/skeletons';
 
 export default function Loading() {

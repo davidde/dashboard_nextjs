8b97bf5f04df60dc4172273c8099a5fe7818911c
David Deprost <dadeprost@gmail.com>
2024-08-28 14:54:47 +0200

    Add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,27 @@
+import CustomersTable from '@/app/ui/customers/table';
+import { Suspense } from 'react';
 import { Metadata } from 'next';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function CustomersPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers: FormattedCustomersTable[] = await fetchFilteredCustomers(query);
+
+    return (
+      <Suspense fallback={<p>Loading...</p>}>
+        <CustomersTable customers={customers}/>
+      </Suspense>
+    )
 }
